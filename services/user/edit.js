0fa977325bbe03106385b2ef8fe132ce5bb42361
avgustine <ilia@kandaurov.su>
2021-09-13 17:12:18 +0300

    barber update

--- services/user/edit.js
+++ services/user/edit.js
@@ -1,41 +1,55 @@
 module.exports = async function (f, opts) {
-    f.post('/edit', {
-        schema: {
-            tags: ['user'],
-            body: {
-                type: 'object',
-                properties: {
-                    name: { type: 'string' },
-                    birthday: { type: 'string' },
-                    photo: { type: 'string' },
-                    frequency: { type: 'string' },
-                    countryId: { type: 'string' },
-                    cityId: { type: 'string' },
-                }
-            },
-            security: [{ 'Authorization': [] }]
+  const Barber = f.db.Barber;
+  f.post(
+    "/edit",
+    {
+      schema: {
+        tags: ["user"],
+        body: {
+          type: "object",
+          properties: {
+            name: { type: "string" },
+            birthday: { type: "string" },
+            photo: { type: "string" },
+            frequency: { type: "string" },
+            countryId: { type: "string" },
+            cityId: { type: "string" },
+          },
         },
-        onRequest: f.auth,
-    }, async (req, res) => {
-	console.log(req.user.id)
-        let {
-            name,
-            birthday,
-            photo,
-            frequency,
-            countryId,
-            cityId, } = req.body;
-	
-        req.user.assign({
-            name,
-            birthday,
-            photo,
-            frequency,
-            countryId,
-            cityId
-        });
-        await req.user.save();
-	
-        res.send(req.user);
-    });
+        security: [{ Authorization: [] }],
+      },
+      onRequest: f.auth,
+    },
+    async (req, res) => {
+      console.log(req.user.id);
+      let { name, birthday, photo, frequency, countryId, cityId } = req.body;
+
+      req.user.assign({
+        name,
+        birthday,
+        photo,
+        frequency,
+        countryId,
+        cityId,
+      });
+      await req.user.save();
+      if (req.body.barber) {
+        const { latitude, longitude, address } = req.body.barber;
+        try {
+          let barber = await Barber.findOne({
+            where: {
+              userId: req.user.id,
+            },
+          });
+          if (barber) {
+            await barber.update({ latitude, longitude, address });
+          }
+        } catch (e) {
+          log.e(e);
+        }
+      }
+
+      res.send(req.user);
+    }
+  );
 };
