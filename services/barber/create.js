d084cc4e79073d64000cc7bc62404cb8ec53886c
avgustine <ilia@kandaurov.su>
2021-09-14 14:31:15 +0300

    Coordinates and address for barber creation

--- services/barber/create.js
+++ services/barber/create.js
@@ -1,44 +1,62 @@
 module.exports = async function (f, opts) {
-    const Barber = f.db.Barber;
-    f.post('/create', {
-        schema: {
-            tags: ['barber'],
-            body: {
-                type: 'object',
-                properties: {
-                    about: { type: 'string' },
-                    orderPlace: { type: 'array', items: { type: 'string' } },
-                    portfolio: { type: 'array', items: { type: 'string' } },
-		    latitude: {type: 'string'},
-		    longitude: {type: 'string'},
-		    address: {type: 'string'}
-                }
-            },
-            security: [{ 'Authorization': [] }]
+  const Barber = f.db.Barber;
+  f.post(
+    "/create",
+    {
+      schema: {
+        tags: ["barber"],
+        body: {
+          type: "object",
+          properties: {
+            about: { type: "string" },
+            orderPlace: { type: "array", items: { type: "string" } },
+            portfolio: { type: "array", items: { type: "string" } },
+            latitude: { type: "string" },
+            longitude: { type: "string" },
+            address: { type: "string" },
+          },
         },
-        onRequest: f.auth,
-    }, async (req, res) => {
-        try {
-            let barber = await Barber.findOne({
-                where: {
-                    userId: req.user.id
-                }
-            });
-            if (barber) return res.methodNotAllowed('You already have registered Barber');
-            let { portfolio, about, orderPlace, latitude, longitude, address } = req.body;
-            barber = await Barber.create({
-                userId: req.user.id,
-                portfolio,
-                about,
-                orderPlace,
-		latitude,
-		longitude,
-		address
-            });
-            res.send(barber);
-        } catch (e) {
-            log.e(e);
+        security: [{ Authorization: [] }],
+      },
+      onRequest: f.auth,
+    },
+    async (req, res) => {
+      try {
+        let barber = await Barber.findOne({
+          where: {
+            userId: req.user.id,
+          },
+        });
+        if (barber)
+          return res.methodNotAllowed("You already have registered Barber");
+        let {
+          portfolio,
+          about,
+          orderPlace,
+          latitude,
+          longitude,
+          address,
+        } = req.body;
+
+        if (req.body.barber) {
+          latitude = req.body.barber.latitude;
+          longitude = req.body.barber.longitude;
+          address = req.body.barber.address;
         }
-        res.internalServerError();
-    });
+        barber = await Barber.create({
+          userId: req.user.id,
+          portfolio,
+          about,
+          orderPlace,
+          latitude,
+          longitude,
+          address,
+        });
+        res.send(barber);
+      } catch (e) {
+        log.e(e);
+      }
+      res.internalServerError();
+    }
+  );
 };
